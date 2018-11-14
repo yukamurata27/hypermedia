1c0dca475544b730688f00a9dd6c56fed05bc663
Yuka Murata <ym26@evansville.edu>
2018-11-14 00:04:08 -0800

    Add button

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,10 @@
 package application;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Slider;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	

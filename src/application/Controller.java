b120393e1d3102172b5fd22f0ebc920a83245cca
Yuka Murata <ym26@evansville.edu>
2018-12-02 14:59:38 -0800

    Add json

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -3,7 +3,13 @@ package application;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Arrays;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -13,16 +19,21 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.Cursor;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Controller {
@@ -33,10 +44,16 @@ public class Controller {
 	private byte[][] green = new byte[HEIGHT][WIDTH];
 	private byte[][] blue = new byte[HEIGHT][WIDTH];
 	
-	private File primaryPath;
-	private String primaryString;
-	private File secondaryPath;
-	private String secondaryString;
+	private File primaryFolder;
+	private String primaryRootPath;
+	private String primaryPath;
+	private File secondaryFolder;
+	private String secondaryRootPath;
+	private String secondaryPath;
+	private double boxX;
+	private double boxY;
+	private double boxW;
+	private double boxH;
 
 	@FXML private Slider sliderL;
 	@FXML private Slider sliderR;
@@ -47,17 +64,17 @@ public class Controller {
 	@FXML private Button importPButton;
 	@FXML private Button importSButton;
 	@FXML private Button makeLinkBtn;
+	@FXML private AnchorPane linkPane;
 
 	public void initialize() throws Exception {
-        // do initialization and configuration work...
-
 		sliderL.valueProperty().addListener(new ChangeListener() {
             @Override
             public void changed(ObservableValue arg0, Object arg1, Object arg2) {
             	int frameNum = (int) sliderL.getValue();
             	textL.setText("Frame " + String.valueOf(frameNum));
             	try {
-            		changeFrame(paneL, createBufferedImg(new File(primaryString + "/" + (primaryString.substring(primaryString.lastIndexOf("/") + 1) + String.format("%04d", frameNum) + ".rgb"))));
+            		primaryPath = primaryRootPath + "/" + (primaryRootPath.substring(primaryRootPath.lastIndexOf("/") + 1) + String.format("%04d", frameNum) + ".rgb");
+            		changeFrame(paneL, createBufferedImg(new File(primaryPath)));
             	} catch (Exception e) {}
             }
         });
@@ -68,29 +85,32 @@ public class Controller {
             	int frameNum = (int) sliderR.getValue();
             	textR.setText("Frame " + String.valueOf(frameNum));
             	try {
-            		changeFrame(paneR, createBufferedImg(new File(secondaryString + "/" + (secondaryString.substring(secondaryString.lastIndexOf("/") + 1) + String.format("%04d", frameNum) + ".rgb"))));
+            		secondaryPath = secondaryRootPath + "/" + (secondaryRootPath.substring(secondaryRootPath.lastIndexOf("/") + 1) + String.format("%04d", frameNum) + ".rgb");
+            		changeFrame(paneR, createBufferedImg(new File(secondaryPath)));
             	} catch (Exception e) {}
             }
         });
     }
 
 	private void setLeftImg (File path) throws Exception {
-		primaryString = path.getAbsolutePath();
-		changeFrame(paneL, createBufferedImg(new File(primaryString + "/" + (primaryString.substring(primaryString.lastIndexOf("/") + 1) + "0001.rgb"))));
+		primaryRootPath = path.getAbsolutePath();
+		primaryPath = primaryRootPath + "/" + (primaryRootPath.substring(primaryRootPath.lastIndexOf("/") + 1) + "0001.rgb");
+		changeFrame(paneL, createBufferedImg(new File(primaryPath)));
 	}
-	
+
 	private void setRightImg (File path) throws Exception {
-		secondaryString = path.getAbsolutePath();
-		changeFrame(paneR, createBufferedImg(new File(secondaryString + "/" + (secondaryString.substring(secondaryString.lastIndexOf("/") + 1) + "0001.rgb"))));
+		secondaryRootPath = path.getAbsolutePath();
+		secondaryPath = secondaryRootPath + "/" + (secondaryRootPath.substring(secondaryRootPath.lastIndexOf("/") + 1) + "0001.rgb");
+		changeFrame(paneR, createBufferedImg(new File(secondaryPath)));
 	}
-	
+
 	private void changeFrame (Pane p, BufferedImage img) {
 		Image i = SwingFXUtils.toFXImage(img, null);
 	    ImageView v = new ImageView(i);
 	    p.getChildren().clear();
 	    p.getChildren().add(v);
 	}
-	
+
 	private BufferedImage createBufferedImg (File file) throws Exception {
 		FileInputStream fileInputStream = null;
 		byte[] stream = new byte[(int) file.length()];
@@ -128,9 +148,9 @@ public class Controller {
 				new EventHandler<ActionEvent>() {
 					@Override
 					public void handle(ActionEvent e) {
-						primaryPath = directoryChooser.showDialog(stage);
+						primaryFolder = directoryChooser.showDialog(stage);
 						try {
-							setLeftImg(primaryPath);
+							setLeftImg(primaryFolder);
 						} catch (Exception error){}
 					}
 				}
@@ -147,25 +167,29 @@ public class Controller {
 				new EventHandler<ActionEvent>() {
 					@Override
 					public void handle(ActionEvent e) {
-						secondaryPath = directoryChooser.showDialog(stage);
+						secondaryFolder = directoryChooser.showDialog(stage);
 						try {
-							setRightImg(secondaryPath);
+							setRightImg(secondaryFolder);
 						} catch (Exception error){}
 					}
 				}
 		);
 	}
-	
+
 	@FXML
 	private void makeNewBox (ActionEvent event) {
-		Rectangle rect = createDraggableRectangle(200, 200, 400, 300);
-        rect.setFill(Color.NAVY);
-
+		boxX = 150;
+		boxY = 130;
+		boxW = 32;
+		boxH = 28;
+		Rectangle rect = createDraggableRectangle(boxX, boxY, boxW, boxH);
+        rect.setFill(Color.TRANSPARENT);
+        rect.setStroke(Color.RED);
         paneL.getChildren().add(rect);
 	}
-	
+
 	private Rectangle createDraggableRectangle(double x, double y, double width, double height) {
-        final double handleRadius = 10 ;
+        final double handleRadius = 10;
 
         Rectangle rect = new Rectangle(x, y, width, height);
 
@@ -200,9 +224,9 @@ public class Controller {
 
         Wrapper<Point2D> mouseLocation = new Wrapper<>();
 
-        setUpDragging(resizeHandleNW, mouseLocation) ;
-        setUpDragging(resizeHandleSE, mouseLocation) ;
-        setUpDragging(moveHandle, mouseLocation) ;
+        setUpDragging(resizeHandleNW, mouseLocation);
+        setUpDragging(resizeHandleSE, mouseLocation);
+        setUpDragging(moveHandle, mouseLocation);
 
         resizeHandleNW.setOnMouseDragged(event -> {
             if (mouseLocation.value != null) {
@@ -211,12 +235,16 @@ public class Controller {
                 double newX = rect.getX() + deltaX ;
                 if (newX >= handleRadius 
                         && newX <= rect.getX() + rect.getWidth() - handleRadius) {
-                    rect.setX(newX);
-                    rect.setWidth(rect.getWidth() - deltaX);
+                	boxX = newX;
+                	boxW = rect.getWidth() - deltaX;
+                	rect.setX(boxX);
+                    rect.setWidth(boxW);
                 }
-                double newY = rect.getY() + deltaY ;
+                double newY = rect.getY() + deltaY;
                 if (newY >= handleRadius 
                         && newY <= rect.getY() + rect.getHeight() - handleRadius) {
+                	boxY = newY;
+                	boxH = rect.getHeight() - deltaY;
                     rect.setY(newY);
                     rect.setHeight(rect.getHeight() - deltaY);
                 }
@@ -228,15 +256,17 @@ public class Controller {
             if (mouseLocation.value != null) {
                 double deltaX = event.getSceneX() - mouseLocation.value.getX();
                 double deltaY = event.getSceneY() - mouseLocation.value.getY();
-                double newMaxX = rect.getX() + rect.getWidth() + deltaX ;
+                double newMaxX = rect.getX() + rect.getWidth() + deltaX;
                 if (newMaxX >= rect.getX() 
                         && newMaxX <= rect.getParent().getBoundsInLocal().getWidth() - handleRadius) {
-                    rect.setWidth(rect.getWidth() + deltaX);
+                    boxW = rect.getWidth() + deltaX;
+                	rect.setWidth(boxW);
                 }
-                double newMaxY = rect.getY() + rect.getHeight() + deltaY ;
+                double newMaxY = rect.getY() + rect.getHeight() + deltaY;
                 if (newMaxY >= rect.getY() 
                         && newMaxY <= rect.getParent().getBoundsInLocal().getHeight() - handleRadius) {
-                    rect.setHeight(rect.getHeight() + deltaY);
+                    boxH = rect.getHeight() + deltaY;
+                	rect.setHeight(boxH);
                 }
                 mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
             }
@@ -250,20 +280,21 @@ public class Controller {
                 double newMaxX = newX + rect.getWidth();
                 if (newX >= handleRadius 
                         && newMaxX <= rect.getParent().getBoundsInLocal().getWidth() - handleRadius) {
-                    rect.setX(newX);
+                    boxX = newX;
+                	rect.setX(boxX);
                 }
                 double newY = rect.getY() + deltaY ;
                 double newMaxY = newY + rect.getHeight();
                 if (newY >= handleRadius 
                         && newMaxY <= rect.getParent().getBoundsInLocal().getHeight() - handleRadius) {
-                    rect.setY(newY);
+                    boxY = newY;
+                	rect.setY(boxY);
                 }
                 mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
             }
-
         });
 
-        return rect ;
+        return rect;
     }
 
     private void setUpDragging(Circle circle, Wrapper<Point2D> mouseLocation) {
@@ -275,9 +306,55 @@ public class Controller {
 
         circle.setOnMouseReleased(event -> {
             circle.getParent().setCursor(Cursor.DEFAULT);
-            mouseLocation.value = null ;
+            mouseLocation.value = null;
         });
     }
 
-    static class Wrapper<T> { T value ; }
+    static class Wrapper<T> { T value; }
+
+    @FXML
+    private void connectVideo (ActionEvent event) throws Exception {
+    	//System.out.println("x: " + boxX + ", y: " + boxY + ", Width: " + boxW + ", Heigh: " + boxH);
+    	//System.out.println("PRIMARY   -> frame: " + ", path: " + primaryPath);
+    	//System.out.println("SECONDARY -> frame: " + ", path: " + secondaryPath);
+    	//System.out.println("SECONDARY -> frame: " + ", path: " + secondaryPath);
+
+    	final Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        //dialog.initOwner(primaryStage);
+        //dialog.initModality(Modality.NONE);
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.getChildren().add(new Text("This is a Dialog"));
+        TextArea textArea = new TextArea(exceptionText);
+        Scene dialogScene = new Scene(dialogVbox, 300, 200);
+        dialog.setScene(dialogScene);
+        dialog.show();
+    	
+    	JSONObject obj1 = new JSONObject();
+    	JSONObject obj2 = new JSONObject();
+    	JSONArray ja = new JSONArray();
+
+    	obj1.put("primary", primaryPath);
+        obj2.put("name", "temp 1");
+        obj2.put("SECONDARY", secondaryPath);
+        obj2.put("x", boxX);
+        obj2.put("y", boxY);
+        obj2.put("width", boxW);
+        obj2.put("height", boxH);
+        obj1.put("frameNo", obj2);
+        ja.add(obj1);
+
+        //StringWriter out = new StringWriter();
+        //obj.writeJSONString(out);
+
+        //String jsonText = out.toString();
+        //System.out.print(jsonText);
+
+        try (FileWriter file = new FileWriter("meta.json")) {
+            file.write(ja.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
